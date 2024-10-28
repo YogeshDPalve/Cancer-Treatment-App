9d0fb440f0182d5c95e63bce5ff36dd4f407d202
Yogesh Palve <dnyyogesh@gmail.com>
2024-10-28 23:34:30 +0530

    profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { redirect, Route, Routes, useNavigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import { Home, Onboarding } from "./pages";
+import { Home, Onboarding, Profile } from "./pages";
+import { useStateContext } from "./context";
+import { usePrivy } from "@privy-io/react-auth";
+import MedicalRecord from './pages/records/Index'
 const App = () => {
+  const { currentUser } = useStateContext();
+  const { user, authenticated, ready, login } = usePrivy();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login();
+    } else if (user && !currentUser) {
+      navigate("/onboarding");
+    }
+  }, [ready, currentUser, navigate]);
+
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
       <div className="relative mr-10 hidden sm:flex">
@@ -17,6 +31,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/medical-records" element={<MedicalRecord />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/records/index.jsx
@@ -0,0 +1,21 @@
+import { IconCirclePlus } from "@tabler/icons-react";
+import React, { useState, useEffect } from "react";
+
+const Index = () => {
+  return (
+    <div className="flex flex-wrap gap-[26px]">
+      <button
+        type="button"
+        className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-neutral-700 bg-[#13131a] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-neutral-800"
+        // onClick={handleOpenModal}
+        onClick={()=>{}}
+
+      >
+        <IconCirclePlus />
+        Create Record
+      </button>
+    </div>
+  );
+};
+
+export default Index;

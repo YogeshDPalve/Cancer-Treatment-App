a8037e1d8883b152b2c7fdd717dbfae8f76716ca
Yogesh Palve <dnyyogesh@gmail.com>
2024-11-01 23:02:30 +0530

    db config

--- src/pages/records/index.jsx
+++ src/pages/records/index.jsx
@@ -11,7 +11,7 @@ const Index = () => {
         // onClick={handleOpenModal}
         onClick={() => {}}
       >
-        <IconCirclePlus />
+        <IconCirclePlus /> 
         Create Record
       </button>
 
